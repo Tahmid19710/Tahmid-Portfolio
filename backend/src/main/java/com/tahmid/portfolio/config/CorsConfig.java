24b54609486a64692ad1c66a395720090721974b
Tahmid19710 <tahmid19710@gmail.com>
2026-09-14 13:54:36 +0600

    fix cors for vercel

--- backend/src/main/java/com/tahmid/portfolio/config/CorsConfig.java
+++ backend/src/main/java/com/tahmid/portfolio/config/CorsConfig.java
@@ -1,51 +1,35 @@
 package com.tahmid.portfolio.config;
 
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
 
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer(){
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
 
 
-        return new WebMvcConfigurer() {
-
-
-            @Override
-            public void addCorsMappings(CorsRegistry registry){
-
-
-                registry.addMapping("/**")
-
-                        .allowedOrigins(
-                                "http://localhost:5173",
-                                "https://tahmid.com"
-                        )
-
-                        .allowedMethods(
-                                "GET",
-                                "POST",
-                                "PUT",
-                                "DELETE"
-                        )
-
-                        .allowedHeaders("*");
-
-
-            }
-
-
-        };
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://tahmid-portfolio-five.vercel.app",
+                        "https://tahmid-portfolio-rcumxsmla-tahmid19710-9576.vercel.app"
+                )
+                .allowedMethods(
+                        "GET",
+                        "POST",
+                        "PUT",
+                        "DELETE",
+                        "OPTIONS"
+                )
+                .allowedHeaders("*")
+                .allowCredentials(true);
 
 
     }
-
 
 }
